0eb1d4a52a7a63e3667dd406e41216266bbe2856
Matheus <matheus_rdl@outlook.com>
2026-05-21 17:25:12 -0300

    Atualizando o console do userSystemServices

--- frontend/src/services/usersSystemServices.jsx
+++ frontend/src/services/usersSystemServices.jsx
@@ -1,6 +1,8 @@
 export default function userSystemServices() {
   const url = `${import.meta.env.VITE_API_URL}/users-system`;
 
+  console.log(import.meta.env.VITE_API_URL)
+
   const getUserSystemByMat = (mat) => {
     return fetch(`${url}/mat/${mat}`).then(res => res.json());
   };
