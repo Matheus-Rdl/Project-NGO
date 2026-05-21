14b23354497eede4d777bc0f1ea454c05297eab9
Matheus <matheus_rdl@outlook.com>
2026-05-21 17:21:52 -0300

    Atualizando o index para o teste render

--- backend/src/index.js
+++ backend/src/index.js
@@ -49,7 +49,7 @@ async function main() {
     res.send({
       success: true,
       statusCode: 200,
-      body: "Welcome to NGO-Project",
+      body: "Welcome to NGO-Project - TESTE",
     });
   });
 
